aa1909b5630648c5b41e0c551645ed071c9ce787
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-22 13:12:33 +0000

    Deploying to gh-pages from @ Natalie-ingineer/goit-js-hw-09@49e8cc7feefe4779609477d5c68d3fad623636ab 🚀

--- 01-color-switcher.ac689f0d.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");function o(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){timerId=setInterval(o(),1e3),n.style.backgroundColor=o()})),e.addEventListener("click",(function(){clearInterval(timerId),console.log("Interval with id ".concat(timerId," has stopped!"))}))}();
-//# sourceMappingURL=01-color-switcher.ac689f0d.js.map
