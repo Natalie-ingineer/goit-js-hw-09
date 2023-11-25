3d12a89e3ece708fefbf86bf9328336116806bc6
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-25 18:57:51 +0000

    Deploying to gh-pages from @ Natalie-ingineer/goit-js-hw-09@47dbcecfb4e02afc4f8e052dce97ca747ba47460 🚀

--- 03-promises.f241f09f.js
+++
@@ -1,2 +0,0 @@
-const o=document.querySelector(".form");console.log(o.delay),console.log(o.step),console.log(o.amount);const e=document.querySelector("button");console.log(e),(void Math.random()).then((({position:o,delay:e})=>{console.log(`✅ Fulfilled promise ${o} in ${e}ms`)})).catch((({position:o,delay:e})=>{console.log(`❌ Rejected promise ${o} in ${e}ms`)}));
-//# sourceMappingURL=03-promises.f241f09f.js.map
