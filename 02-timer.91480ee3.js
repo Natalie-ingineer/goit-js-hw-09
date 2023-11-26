6a8134f7f1c1b3c99308e40e3dd2b3e9eafe656b
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2023-11-26 08:56:38 +0000

    Deploying to gh-pages from @ Natalie-ingineer/goit-js-hw-09@a34f6e1de95037dcf1db8e5c4380b45e1df3160c 🚀

--- 02-timer.91480ee3.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t=1e3,n=function(e){document.querySelector("span").innerText=e};e=new Date("2024/06/03"),setInterval((function(){var o=new Date(e)-new Date,a=Math.floor(o/t%60),c=Math.floor(o/6e4%60),r=Math.floor(o/36e5%24),f=Math.floor(o/864e5),l="".concat(f,"d ").concat(r,":").concat(c,":").concat(a);n(l)}),1e3)}();
-//# sourceMappingURL=02-timer.91480ee3.js.map
--- 02-timer.b305bac4.js
+++
@@ -1,2 +0,0 @@
-const e=e=>{document.querySelector("span").innerText=e};var t;t=new Date("2024/06/03"),setInterval((()=>{const o=new Date(t)-new Date,a=Math.floor(o/1e3%60),n=Math.floor(o/6e4%60),r=Math.floor(o/36e5%24),l=Math.floor(o/864e5);e(`${l}d ${r}:${n}:${a}`)}),1e3);
-//# sourceMappingURL=02-timer.b305bac4.js.map
